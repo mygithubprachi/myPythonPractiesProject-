ddce035c21a4664aba572f429d2743f51bb4c9fd
PrachiPatil <patilprachi937@gmail.com>
2025-08-28 21:59:10 +0530

    Create list.py
    created a student list

---
+++ list.py
@@ -0,0 +1,11 @@
+list=[23,45,67,98,89]
+print("this is sorted list",list.sort)
+list1=list
+print("this is reversed list",list.reverse)
+name=input("enter users name")
+studentList=[]
+for i in range(0,11):
+    # print("enter student name")
+     studentList.append(input("enter student name"))
+
+print(studentList)
