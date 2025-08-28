5893f13621fb893f6506f5954b61fdfc0b2697c5
PrachiPatil <patilprachi937@gmail.com>
2025-08-28 23:58:39 +0530

    Create class.py
    class created

---
+++ class.py
@@ -0,0 +1,15 @@
+class Home:
+    def __init__(self,s,c,n):
+        self.size=s 
+        self.color=c
+        self.name=n 
+
+    def structure(self):
+        print("My Home size is ",self.size,"and color ",self.color)
+    
+    def co(self):
+        print("My Home size is ",self.size,"and color ",self.color)
+
+h1=Home("20sqfee","red","Aastha bhawan")
+h1.structure()
+h1.co()
