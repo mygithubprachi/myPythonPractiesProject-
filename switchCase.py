e1b5c48ddc3d126725bd9cfe583a26266bf0bfac
PrachiPatil <patilprachi937@gmail.com>
2025-08-28 22:09:32 +0530

    Create switchCase.py
    created a switch case

---
+++ switchCase.py
@@ -0,0 +1,11 @@
+choice =int(input("Enter a number"))
+match choice:
+    case 1:
+        print("you choose one")
+    case 2:
+        print("you choose two")
+    case 3:
+        print("you choose three")
+    case _:
+        print("invalid choice")
+           
